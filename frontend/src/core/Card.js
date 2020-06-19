d4da54989f2c52626b45852372fc00b1f7c7df3a
s-katte <shubhamkatte99@gmail.com>
2020-06-19 10:15:00 +0530

    Added items to cart

--- frontend/src/core/Card.js
+++ frontend/src/core/Card.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState, Redirect } from "react";
 import ImageHelper from "./ImageHelper";
+import { addItemToCart } from "./helper/CartHelper";
 
 const Card = ({ product, addtoCart = true, removeFromCart = false }) => {
+  const [redirect, setRedirect] = useState(false);
+  const [count, setCount] = useState(product.count);
+
   const cardTitle = product ? product.name : "A photo from pexels";
   const cardDescription = product ? product.description : "Default description";
   const cardPrice = product ? product.price : "DEFAULT";
+
+  const addToCart = () => {
+    addItemToCart(product, () => setRedirect(true));
+  };
+
+  const getARedirect = (redirect) => {
+    if (redirect) {
+      return <Redirect to={"/cart/"} />;
+    }
+  };
 
   const showAddToCart = (addtoCart) => {
     return (
       addtoCart && (
         <button
-          onClick={() => {}}
+          onClick={addToCart}
           className="btn btn-block btn-outline-success mt-2 mb-2"
         >
           Add to Cart
@@ -36,6 +50,7 @@ const Card = ({ product, addtoCart = true, removeFromCart = false }) => {
     <div className="card text-white bg-dark border border-info ">
       <div className="card-header lead">{cardTitle}</div>
       <div className="card-body">
+        {getARedirect(redirect)}
         <ImageHelper product={product} />
         <p className="lead bg-success font-weight-normal text-wrap">
           {cardDescription}
---
+++ frontend/src/core/helper/CartHelper.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+export const addItemToCart = (item, next) => {
+  let cart = [];
+  if (typeof window != undefined) {
+    if (localStorage.getItem("cart")) {
+      cart = JSON.parse(localStorage.getItem("cart"));
+    }
+    cart.push({
+      ...item,
+      count: 1,
+    });
+    localStorage.setItem("cart", JSON.stringify(cart));
+    next();
+  }
+};
